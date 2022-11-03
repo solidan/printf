1e71edadb6c75a280fdfb57c88524b44b6190b83
solidan <105629931+solidan@users.noreply.github.com>
2022-11-03 12:35:38 +0100

    commit

--- libftprintf.a/libprintf/ft_printf_utils.c
+++ libftprintf.a/libprintf/ft_printf_utils.c
@@ -6,21 +6,19 @@
 /*   By: acuesta- <acuesta-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 12:55:55 by acuesta-          #+#    #+#             */
-/*   Updated: 2022/11/02 13:09:36 by acuesta-         ###   ########.fr       */
+/*   Updated: 2022/11/03 11:16:20 by acuesta-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
 
 #include <stdlib.h>
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void ft_putchar(char c)
 {
-	write (1, &c, 2);
+	write(1, &c, 2);
 }
 
-void	ft_putstr(char *str)
+void ft_putstr(char *str)
 {
 	int i;
 
@@ -33,10 +31,49 @@ void	ft_putstr(char *str)
 	}
 }
 
+
+int	ft_printstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str == NULL)
+	{
+		ft_putstr("(null)");
+		return (6);
+	}
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	return (i);
+}
+
+int	ft_printnbr(int n)
+{
+	int		len;
+	char	*num;
+
+	len = 0;
+	num = ft_itoa(n);
+	len = ft_printstr(num);
+	free(num);
+	return (len);
+}
+
+
+int	ft_printpercent(void)
+{
+	write(1, "%", 1);
+	return (1);
+}
+
+
+
 int main(int argc, const char *argv[])
 {
-	char str[] = {'H' ,'e' ,'l' ,'l' ,'o' ,' ' ,'w' ,'o' ,'r' ,'l' ,'d', '\n'};
+	char str[] = {'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n'};
 	ft_putstr(str);
 	return 0;
 }
-
